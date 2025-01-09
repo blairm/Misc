2b0ce8b1b28e573e98cc0b81c01002be8288184a
blairm <blairm@users.noreply.github.com>
2025-01-09 14:04:08 +0000

    open project file for linux and windows

---
+++ KDTree/OpenProject_Win32.bat
@@ -0,0 +1,8 @@
+@echo off
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall" x64
+
+for %%F in ("%cd%") do set "folder=%%~nxF"
+start /b "sublime" "%ProgramFiles%\Sublime Text 3\sublime_text.exe" --project %folder%.sublime-project -n
+
+call devenv Build\main.exe
